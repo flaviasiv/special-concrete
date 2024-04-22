eb5d854836f3d2b69651f7efaa4810b583052f33
flavia sigoli <flaviasivm@gmail.com>
2024-04-22 18:33:41 -0300

    initial commit

---
+++ js/javascript.js
@@ -0,0 +1,33 @@
+//navlinks menu mobile
+let menu_icon_box = document.querySelector(".btn-toggle");
+let box = document.querySelector(".box");
+
+menu_icon_box.onclick = function(){
+    menu_icon_box.classList.toggle("active");
+    box.classList.toggle("active_box");
+}
+document.onclick = function(e){
+    if (!menu_icon_box.contains(e.target) && !box.contains(e.target) ) {
+        menu_icon_box.classList.remove("active");
+        box.classList.remove("active_box");
+    }
+}
+
+// scroll behavior
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
